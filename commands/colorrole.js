7f0bb28263bc824ac3b539b102d541b5c984e9e0
Sam McNeely <4legsam@gmail.com>
2020-06-17 19:30:47 -0700

    Verifiy that user has permissions to run init

--- commands/colorrole.js
+++ commands/colorrole.js
@@ -11,8 +11,9 @@ module.exports = {
         console.log()
         if(args[0] === "init") {
             console.log('init')
-            // console.log(message.guild.roles.cache)
-            for (const color of colors) {
+            if(message.member.hasPermission("ADMINISTRATOR")) {
+                // console.log(message.guild.roles.cache)
+                for (const color of colors) {
                 if(message.guild.roles.cache.find(Role => Role.name === color.name)) {
                     console.log(`color role: ${color.name} already exists`);
                 } else {
@@ -25,7 +26,11 @@ module.exports = {
                         })
                         .then(console.log(`New color added ${color.name}`))
                         .catch(console.error);
+                    }
                 }
+            } else {
+                message.reply(`, You dont have the permissions to run this command!`);
+                console.log(`User doesn't have permissions.`);
             }
         }
     }
